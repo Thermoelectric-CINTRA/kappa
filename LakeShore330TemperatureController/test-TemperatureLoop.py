e283dc8c040df58c131c325a144e9f3a689d6c64
Thermoelectric-CINTRA <thermoelectric.cintra@gmail.com>
2021-02-21 13:25:21 +0800

    Adjust PID

--- LakeShore330TemperatureController/test-TemperatureLoop.py
+++ LakeShore330TemperatureController/test-TemperatureLoop.py
@@ -38,63 +38,103 @@ def TempLoop():
             temperatureController.heater_range = "medium"
         elif setpoint <= 20 and setpoint > 15:
             temperatureController.gain = 1
-            temperatureController.reset = 999
-            temperatureController.rate = 200
+            temperatureController.reset = 900
+            temperatureController.rate = 0
             temperatureController.heater_range = "high"
         elif setpoint <= 25 and setpoint > 20:
             temperatureController.gain = 1
-            temperatureController.reset = 900
-            temperatureController.rate = 200
-            temperatureController.heater_range = "high"
-        elif setpoint <= 30 and setpoint > 25:
-            temperatureController.gain = 10
-            temperatureController.reset = 900
+            temperatureController.reset = 100
             temperatureController.rate = 0
             temperatureController.heater_range = "high"
-        elif setpoint <= 35 and setpoint > 30:
-            temperatureController.gain = 10
-            temperatureController.reset = 900
-            temperatureController.rate = 0
-            temperatureController.heater_range = "high"
-        elif setpoint <= 40 and setpoint > 35:
-            temperatureController.gain = 20
-            temperatureController.reset = 900
-            temperatureController.rate = 0
-            temperatureController.heater_range = "high"
-        elif setpoint <= 45 and setpoint > 40:
-            temperatureController.gain = 20
-            temperatureController.reset = 900
-            temperatureController.rate = 0
-            temperatureController.heater_range = "high"
-        elif setpoint <= 50 and setpoint > 45:
-            temperatureController.gain = 30
-            temperatureController.reset = 900
-            temperatureController.rate = 0
-            temperatureController.heater_range = "high"
-        elif setpoint <= 55 and setpoint > 50:
-            temperatureController.gain = 30
-            temperatureController.reset = 900
-            temperatureController.rate = 0
-            temperatureController.heater_range = "high"
-        elif setpoint <= 60 and setpoint > 55:
-            temperatureController.gain = 40
-            temperatureController.reset = 900
-            temperatureController.rate = 0
-            temperatureController.heater_range = "high"
-        elif setpoint <= 65 and setpoint > 60:
-            temperatureController.gain = 40
-            temperatureController.reset = 900
-            temperatureController.rate = 0
-            temperatureController.heater_range = "high"
-        elif setpoint <= 70 and setpoint > 65:
-            temperatureController.gain = 50
-            temperatureController.reset = 900
-            temperatureController.rate = 0
-            temperatureController.heater_range = "high"
+        # elif setpoint <= 60 and setpoint > 45:
+        #     temperatureController.gain =3
+        #     temperatureController.reset = 900
+        #     temperatureController.rate = 200
+        #     temperatureController.heater_range = "high"
+        # elif setpoint <= 75 and setpoint > 60:
+        #     temperatureController.gain = 15
+        #     temperatureController.reset = 900
+        #     temperatureController.rate = 200
+        #     temperatureController.heater_range = "high"
+        # elif setpoint <= 90 and setpoint > 75:
+        #     temperatureController.gain = 20
+        #     temperatureController.reset = 900
+        #     temperatureController.rate = 0
+        #     temperatureController.heater_range = "high"
+        # elif setpoint <= 105 and setpoint > 90:
+        #     temperatureController.gain = 30
+        #     temperatureController.reset = 900
+        #     temperatureController.rate = 200
+        #     temperatureController.heater_range = "high"
+        # elif setpoint <= 120 and setpoint > 105:
+        #     temperatureController.gain = 40
+        #     temperatureController.reset = 900
+        #     temperatureController.rate = 200
+        #     temperatureController.heater_range = "high"
+        # elif setpoint <= 135 and setpoint > 120:
+        #     temperatureController.gain = 50
+        #     temperatureController.reset = 900
+        #     temperatureController.rate = 200
+        #     temperatureController.heater_range = "high"
+        # elif setpoint <= 150 and setpoint > 135:
+        #     temperatureController.gain = 60
+        #     temperatureController.reset = 900
+        #     temperatureController.rate = 200
+        #     temperatureController.heater_range = "high"
+        # elif setpoint <= 165 and setpoint > 150:
+        #     temperatureController.gain = 70
+        #     temperatureController.reset = 900
+        #     temperatureController.rate = 200
+        #     temperatureController.heater_range = "high"
+        # elif setpoint <= 180 and setpoint > 165:
+        #     temperatureController.gain = 80
+        #     temperatureController.reset = 900
+        #     temperatureController.rate = 200
+        #     temperatureController.heater_range = "high"
+        # elif setpoint <= 195 and setpoint > 180:
+        #     temperatureController.gain = 90
+        #     temperatureController.reset = 900
+        #     temperatureController.rate = 200
+        #     temperatureController.heater_range = "high"
+        # elif setpoint <= 210 and setpoint > 195:
+        #     temperatureController.gain = 100
+        #     temperatureController.reset = 900
+        #     temperatureController.rate = 200
+        #     temperatureController.heater_range = "high"
+        # elif setpoint <= 225 and setpoint > 210:
+        #     temperatureController.gain = 110
+        #     temperatureController.reset = 900
+        #     temperatureController.rate = 200
+        #     temperatureController.heater_range = "high"
+        # elif setpoint <= 240 and setpoint > 225:
+        #     temperatureController.gain = 120
+        #     temperatureController.reset = 900
+        #     temperatureController.rate = 200
+        #     temperatureController.heater_range = "high"
+        # elif setpoint <= 255 and setpoint > 240:
+        #     temperatureController.gain = 130
+        #     temperatureController.reset = 900
+        #     temperatureController.rate = 200
+        #     temperatureController.heater_range = "high"
+        # elif setpoint <= 270 and setpoint > 255:
+        #     temperatureController.gain = 140
+        #     temperatureController.reset = 900
+        #     temperatureController.rate = 200
+        #     temperatureController.heater_range = "high"
+        # elif setpoint <= 285 and setpoint > 270:
+        #     temperatureController.gain = 150
+        #     temperatureController.reset = 900
+        #     temperatureController.rate = 200
+        #     temperatureController.heater_range = "high"
+        # elif setpoint <= 300 and setpoint > 285:
+        #     temperatureController.gain = 160
+        #     temperatureController.reset = 900
+        #     temperatureController.rate = 200
+        #     temperatureController.heater_range = "high"
         else:
             temperatureController.heater_range = "off"
 
-    setpoint = 10
+    setpoint = 15
     temperatureController.setpoint = setpoint
     PID(setpoint)
     step = 5
@@ -121,7 +161,7 @@ def TempLoop():
         if setpoint >= temperature - 0.1 and setpoint <= temperature + 0.1:
             hold_time += 1
             print("Hold time of {}K in second: {}".format(temperature, hold_time))
-            if hold_time >= 120:
+            if hold_time >= 60:
                 # RecordOhm(temperature)
                 time.sleep(1)
                 setpoint += step
@@ -158,7 +198,7 @@ def LivePlot():
         elif x_values[len(x_values)-1] > 3600 :
             plt.xlabel('Time(hours)')
             plt.plot(x_values/3600, y_values)
-            plt.plot(x_values/60, setpoint_values)
+            plt.plot(x_values/3600, setpoint_values)
             plt.plot(x_values/3600, setpoint_values+0.1)
             plt.plot(x_values/3600, setpoint_values-0.1)
         else:
